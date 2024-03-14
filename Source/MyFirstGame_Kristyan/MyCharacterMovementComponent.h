dad52c8a1203f07efa4256e5cf3cc5a2051bdc5c
kriskross2018 <46082434+kriskross2018@users.noreply.github.com>
2024-03-15 00:17:33 +0700

    Task 3
    Survival game

---
+++ Source/MyFirstGame_Kristyan/MyCharacterMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MyCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTGAME_KRISTYAN_API UMyCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MyFirstGame_Kristyan/MyFirstGame_Kristyan.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyFirstGame_Kristyan.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyFirstGame_Kristyan, "MyFirstGame_Kristyan" );
